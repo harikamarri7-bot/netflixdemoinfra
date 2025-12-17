9f23190e85f84cdf5b49863aff90ee8e5b406067
devopsbyharika <harikamarri7@gmail.com>
2025-12-17 19:09:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-00839c71d8f6096b4"
+  ami                    = "ami-0754c6e75b3b97dcd"
   instance_type          = "t2.medium"
   key_name               = "jenkins"
   vpc_security_group_ids = ["sg-0b646936fb0ea432f"]
