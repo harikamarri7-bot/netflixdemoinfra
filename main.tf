a374138dfb97cd797b721c0aca8809030438c2c6
devopsbyharika <harikamarri7@gmail.com>
2025-12-17 18:09:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "monitoring-server"]
-}
-
-resource "aws_instance" "two" {
-  count                  = length(var.instance_names)
-  ami                    = "ami-051e483428ae60e7d"  
-  instance_type          = "t3.medium"
+resource "aws_instance" "one" {
+  count                  = 4
+  ami                    = "ami-0c398cb65a93047f2"
+  instance_type          = "t2.medium"
   key_name               = "jenkins"
   vpc_security_group_ids = ["sg-0b646936fb0ea432f"]
-
   tags = {
     Name = var.instance_names[count.index]
   }
+}
+
+variable "instance_names" {
+  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
