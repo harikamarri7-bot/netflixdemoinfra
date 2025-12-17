71f93d714a410ae51ba2dbe9f3a87ea4bbcbe427
devopsbyharika <harikamarri7@gmail.com>
2025-12-17 19:29:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -18,10 +9,10 @@ variable "instance_names" {
 
 resource "aws_instance" "one" {
   count                  = length(var.instance_names)
-  ami                    = "ami-0754c6e75b3b97dcd"
+  ami                    = "ami-00839c71d8f6096b4"
   instance_type          = "t2.medium"
   key_name               = "jenkins"
-  vpc_security_group_ids = ["sg-0b646936fb0ea432f"]
+  vpc_security_group_ids = ["sg-09b5bbfbc272eae41"]
 
   tags = {
     Name = var.instance_names[count.index]
