9d237ba7032df6588721d113c5f73c9bff964c59
devopsbyharika <harikamarri7@gmail.com>
2025-12-17 17:29:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ variable "instance_names" {
 
 resource "aws_instance" "two" {
   count                  = length(var.instance_names)
-  ami                    = "ami-005b1eaf15c2e3e20"  # ✅ Ubuntu 22.04 LTS (eu-north-1)
-  instance_type          = "t3.medium"
-  key_name               = "slave1"
-  vpc_security_group_ids = ["sg-07497a48ddb5a2d1f"]
+  ami                    = "ami-00839c71d8f6096b4"  # ✅ Ubuntu 22.04 LTS (eu-north-1)
+  instance_type          = "t2.medium"
+  key_name               = "jenkins"
+  vpc_security_group_ids = ["sg-0b646936fb0ea432f"]
 
   tags = {
     Name = var.instance_names[count.index]
